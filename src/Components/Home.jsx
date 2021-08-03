c4bdadcb6273c875a7d8d99375a9bad82ba82d0c
Augusto Souza <augustojsouza1@gmail.com>
2021-08-02 21:16:10 -0300

    first commit

---
+++ src/Components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import * as Styles from './styles';
+
+const Home = () => {
+
+    return (
+        <div>
+            <Styles.Title backgroundColor="tomato" titleColor="white" >
+                It's Live!
+            </Styles.Title>
+        </div>
+    )
+}
+
+export default Home;
