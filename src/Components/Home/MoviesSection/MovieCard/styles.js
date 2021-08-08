01490f0f444d7acb808d716746ffa2952ba9c00e
Augusto Souza <augustojsouza1@gmail.com>
2021-08-08 12:14:57 -0300

    Created hover effect for MovieCard component

--- src/Components/Home/MoviesSection/MovieCard/styles.js
+++ src/Components/Home/MoviesSection/MovieCard/styles.js
@@ -5,6 +5,11 @@ const MoviePoster = styled.img`
   max-height: 20rem;
   margin: 0.5rem;
   border-radius: 3px;
+  &:hover {
+    -webkit-transform: scale(1.1);
+    -ms-transform: scale(1.1);
+    transform: scale(1.1);
+  }
 `;
 
 export default MoviePoster;
