bdda316c24c1737fb18653a41687652a599b3fae
Augusto Souza <augustojsouza1@gmail.com>
2021-08-08 10:34:57 -0300

    Finished styling the NavLinks

--- src/Components/Home/MoviesSection/MoviesSection.jsx
+++ src/Components/Home/MoviesSection/MoviesSection.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
-import { MovieSectionWrapper, MovieSectionNav } from './styles';
+import { Routes, Route, NavLink } from 'react-router-dom';
+import {
+  MovieSectionWrapper,
+  MovieSectionNav,
+  MovieCardsWrapper,
+  NavigationLinks,
+} from './styles';
 import TrendingMovies from './TrendingMovies/TrendingMovies';
 import TopRatedMovies from './TopRatedMovies/TopRatedMovies';
 import NewArrivalsMovies from './NewArrivalsMovies/NewArrivalsMovies';
@@ -9,17 +14,32 @@ import MoviesGenreDropdown from './MoviesGenre/MoviesGenreDropdown';
 const MoviesSection = () => (
   <MovieSectionWrapper>
     <MovieSectionNav>
-      <Link to="/trending">Trending</Link>
-      <Link to="/top-rated">Top Rated</Link>
-      <Link to="/new-arrivals">New Arrivals</Link>
-      <Link to="/genre">Genre</Link>
+      <NavigationLinks>
+        <NavLink to="/trending" activeClassName="current">
+          <li>Trending</li>
+        </NavLink>
+
+        <NavLink to="/top-rated" activeClassName="current">
+          <li>Top Rated</li>
+        </NavLink>
+
+        <NavLink to="/new-arrivals" activeClassName="current">
+          <li>New Arrivals</li>
+        </NavLink>
+
+        <NavLink to="/genre" activeClassName="current">
+          <li>Genre</li>
+        </NavLink>
+      </NavigationLinks>
+    </MovieSectionNav>
+    <MovieCardsWrapper>
       <Routes>
         <Route path="/trending" element={<TrendingMovies />} />
         <Route path="/top-rated" element={<TopRatedMovies />} />
         <Route path="/new-arrivals" element={<NewArrivalsMovies />} />
         <Route path="/genre" element={<MoviesGenreDropdown />} />
       </Routes>
-    </MovieSectionNav>
+    </MovieCardsWrapper>
   </MovieSectionWrapper>
 );
 
--- src/Components/Home/MoviesSection/styles.js
+++ src/Components/Home/MoviesSection/styles.js
@@ -3,10 +3,32 @@ import styled from 'styled-components';
 export const MovieSectionWrapper = styled.section`
   max-width: 60rem;
   margin: 0 auto;
-  border: 1px solid red;
+`;
+export const NavigationLinks = styled.ul`
+  display: flex;
+
+  a {
+    text-decoration: none;
+  }
+
+  li {
+    margin-right: 1rem;
+  }
+
+  .current {
+    li {
+      color: #ec407a;
+      border-bottom: 2px solid #ec407a;
+    }
+  }
 `;
 
 export const MovieSectionNav = styled.nav`
-  margin: 3rem 0;
+  display: flex;
+  margin: 2rem 0;
   font-weight: bold;
+`;
+
+export const MovieCardsWrapper = styled.div`
+  display: flex;
 `;
