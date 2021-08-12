6ae070e76d6985c423c7ba475e5c16ce3b6ad757
Augusto Souza <augustojsouza1@gmail.com>
2021-08-12 20:59:29 -0300

    Fetch cast of movie in MovieCast component

--- src/Components/Home/MoviesSection/MovieDetails/MovieCast/MovieCast.jsx
+++ src/Components/Home/MoviesSection/MovieDetails/MovieCast/MovieCast.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { CastWrapper, CastTitle } from './styles';
+import { GET_CAST, POSTER_URL } from '../../../../../Services/api';
+import useAxios from '../../../../../Hooks/useAxios';
+import Loading from '../../../../Helper/Loading/Loading';
+import Error from '../../../../Helper/Error/Error';
 
 const MovieCast = ({ movieId }) => {
-  console.log(movieId);
+  const { data, loading, error, request } = useAxios();
 
-  return (
-    <div>
-      <h1>Cast movie {movieId}</h1>
-    </div>
-  );
+  React.useEffect(() => {
+    async function getCastByMovieId(id) {
+      const url = GET_CAST(id);
+      await request(url);
+    }
+
+    getCastByMovieId(movieId);
+  }, []);
+
+  if (error) return <Error errorLog={error} />;
+  if (loading) return <Loading />;
+  if (data)
+    return (
+      <CastWrapper>
+        <CastTitle>Cast movie {movieId}</CastTitle>
+      </CastWrapper>
+    );
+  return null;
 };
 
 MovieCast.propTypes = {
---
+++ src/Components/Home/MoviesSection/MovieDetails/MovieCast/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const CastWrapper = styled.section`
+  padding: 2rem 0;
+  max-width: 60rem;
+  display: flex;
+  margin: 0 auto;
+`;
+
+export const CastTitle = styled.h1`
+  font-size: 3rem;
+`;
