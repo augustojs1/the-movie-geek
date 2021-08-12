7c537e36a62f569aaa0cbb81615fbca71ab4d5c9
Augusto Souza <augustojsouza1@gmail.com>
2021-08-12 20:05:31 -0300

    Add movie runtime in hours

--- src/Components/Home/MoviesSection/MovieDetails/MovieInformation/MovieInformation.jsx
+++ src/Components/Home/MoviesSection/MovieDetails/MovieInformation/MovieInformation.jsx
@@ -11,12 +11,26 @@ import {
 } from './styles';
 import MovieRating from './MovieRating/MovieRating';
 
-const MovieInformation = ({ title, overview, releaseDate, rating }) => {
+const MovieInformation = ({
+  title,
+  overview,
+  releaseDate,
+  rating,
+  runtime,
+}) => {
   function getReleaseYear(movieReleaseDate) {
     return movieReleaseDate.slice(0, 4);
   }
 
   const releaseYear = getReleaseYear(releaseDate);
+
+  function getRuntimeInHours(runtimeInMinutes) {
+    const hours = Math.floor(runtimeInMinutes / 60);
+    const minutes = runtimeInMinutes % 60;
+    return `${hours}h ${minutes}m`;
+  }
+
+  const runtimeInHour = getRuntimeInHours(runtime);
 
   return (
     <MovieInformationWrapper>
@@ -27,7 +41,7 @@ const MovieInformation = ({ title, overview, releaseDate, rating }) => {
           <TitleText>IMDB Rating</TitleText> <MovieRating rating={rating} />
         </RatingWrapper>
         <RuntimeWrapper>
-          <TitleText>Runtime</TitleText> 1h 45m
+          <TitleText>Runtime</TitleText> {runtimeInHour}
         </RuntimeWrapper>
       </Wrapper>
     </MovieInformationWrapper>
@@ -39,6 +53,7 @@ MovieInformation.propTypes = {
   overview: PropTypes.string.isRequired,
   releaseDate: PropTypes.string.isRequired,
   rating: PropTypes.string.isRequired,
+  runtime: PropTypes.number.isRequired,
 };
 
 export default MovieInformation;
